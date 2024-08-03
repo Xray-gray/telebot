6e72cf44fc94b2339b7340fadb54f3403d40adf5
Vardanyan Maria <narine.hakobyan.1997@gmail.com>
2024-08-03 17:14:33 +0400

    Your commit message

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
 import random
 import logging
-from telegram import Update
+from flask import Flask, request
+from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from telegram.ext import CallbackContext
 
-# Fetch token from environment variable
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 VIDEO_FOLDER = 'videos/barev/'
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
+app = Flask(__name__)
+bot = Bot(token=TOKEN)
+application = Application.builder().token(TOKEN).build()
+
 async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text('Բարև, ես բոտ եմ, ով բարևում է բոլոր նոր մասնակիցներին :)')
+    await update.message.reply_text('Բարև, ես Ջարվիս բոտն եմ, ով բարևում է բոլոր նոր մասնակիցներին :)')
 
 async def new_member(update: Update, context: CallbackContext) -> None:
     if update.message.new_chat_members:
@@ -30,19 +34,19 @@ async def new_member(update: Update, context: CallbackContext) -> None:
                     await context.bot.send_video(chat_id=update.message.chat_id, video=video)
             except Exception as e:
                 logging.error(f"Error sending video: {str(e)}")
-                await update.message.reply_text(f'Произошла ошибка при отправке видео: {str(e)}')
+                await update.message.reply_text(f'Վիդեոն ուրիշ անգամ: {str(e)}')
 
-def main():
-    token = os.getenv('TELEGRAM_BOT_TOKEN')
-    if not token:
-        raise ValueError("No TELEGRAM_BOT_TOKEN environment variable set")
+@app.route('/' + TOKEN, methods=['POST'])
+def webhook():
+    json_str = request.get_data(as_text=True)
+    update = Update.de_json(json_str, bot)
+    application.process_update(update)
+    return 'OK'
 
-    application = Application.builder().token(token).build()
-
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, new_member))
-
-    application.run_polling()  # Here, `await` is not needed
+def set_webhook():
+    webhook_url = f"https://your-render-url.com/{TOKEN}"
+    bot.set_webhook(url=webhook_url)
 
 if __name__ == '__main__':
-    main()
+    set_webhook()
+    app.run(host='0.0.0.0', port=80)
