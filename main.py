3f866240aac29fb1157d0325698cd60083a4a684
Vardanyan Maria <narine.hakobyan.1997@gmail.com>
2024-08-03 16:39:16 +0400

    Your commit message

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ VIDEO_FOLDER = 'videos/barev/'
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
+
 async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text('Բարև, ես JARVIS բոտն եմ, ով բարևում է բոլոր նոր մասնակիցներին :)')
+    await update.message.reply_text('Բարև, ես բոտ եմ, ով բարևում է բոլոր նոր մասնակիցներին :)')
+
 
 async def new_member(update: Update, context: CallbackContext) -> None:
     if update.message.new_chat_members:
@@ -29,15 +31,21 @@ async def new_member(update: Update, context: CallbackContext) -> None:
                     await context.bot.send_video(chat_id=update.message.chat_id, video=video)
             except Exception as e:
                 logging.error(f"Error sending video: {str(e)}")
-                await update.message.reply_text(f'Վիդեոն հետո: {str(e)}')
+                await update.message.reply_text(f'Произошла ошибка при отправке видео: {str(e)}')
+
 
 def main():
-    application = Application.builder().token(TOKEN).build()
+    token = os.getenv('7476413517:AAFfOQzIfWIRKLif2zcAsEPMNHzmwORTO6Y')
+    if not token:
+        raise ValueError("No TELEGRAM_BOT_TOKEN environment variable set")
+
+    application = Application.builder().token(token).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, new_member))
 
     application.run_polling()  # Here, `await` is not needed
+
 
 if __name__ == '__main__':
     main()
