72e9054fccc9618a447609db6a742912623f51e9
Vardanyan Maria <narine.hakobyan.1997@gmail.com>
2024-08-03 14:07:19 +0400

    Resolved merge conflicts

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 worker: python main.py
-=======
-worker: python main.py
->>>>>>> 341398790e2c843074a96f9ecdfbbc5201d3d037
--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 import os
 import random
+import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from telegram.ext import CallbackContext
 
-TOKEN = '7476413517:AAFfOQzIfWIRKLif2zcAsEPMNHzmwORTO6Y'  # Use a new token if needed
+TOKEN = '7476413517:AAFfOQzIfWIRKLif2zcAsEPMNHzmwORTO6Y'
 VIDEO_FOLDER = 'videos/barev/'
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('Բարև, ես բոտ եմ, ով բարևում է բոլոր նոր մասնակիցներին :)')
 
 async def new_member(update: Update, context: CallbackContext) -> None:
-    for member in update.message.new_chat_members:
-        await update.message.reply_text(f'Բարի գալուստ, {member.full_name}!')
-        video_file = random.choice(os.listdir(VIDEO_FOLDER))
-        video_path = os.path.join(VIDEO_FOLDER, video_file)
-        with open(video_path, 'rb') as video:
-            await context.bot.send_video(chat_id=update.message.chat_id, video=video)
+    if update.message.new_chat_members:
+        for member in update.message.new_chat_members:
+            logging.info(f"New member detected: {member.full_name}")
+            await update.message.reply_text(f'Բարի գալուստ, {member.full_name}!')
+
+            video_file = random.choice(os.listdir(VIDEO_FOLDER))
+            video_path = os.path.join(VIDEO_FOLDER, video_file)
+            logging.info(f"Selected video: {video_file}")
+
+            try:
+                with open(video_path, 'rb') as video:
+                    await context.bot.send_video(chat_id=update.message.chat_id, video=video)
+            except Exception as e:
+                logging.error(f"Error sending video: {str(e)}")
+                await update.message.reply_text(f'Произошла ошибка при отправке видео: {str(e)}')
 
 async def main():
+    # Проверка пути к видео и доступности файлов
+    logging.info(f"Video folder path: {os.path.abspath(VIDEO_FOLDER)}")
+    logging.info(f"Files in video folder: {os.listdir(VIDEO_FOLDER)}")
+
     application = Application.builder().token(TOKEN).build()
+
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, new_member))
+
     await application.run_polling()
 
 if __name__ == '__main__':
