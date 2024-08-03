83410c5cdb8f714275d8677425abe6fc93b14004
Vardanyan Maria <narine.hakobyan.1997@gmail.com>
2024-08-03 12:44:00 +0400

    Describe your changes here

--- main.py
+++ main.py
@@ -1,42 +1,29 @@
 import os
 import random
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from telegram.ext import CallbackContext
 
-TOKEN = '7476413517:AAFfOQzIfWIRKLif2zcAsEPMNHzmwORTO6Y'
+TOKEN = '7476413517:AAFfOQzIfWIRKLif2zcAsEPMNHzmwORTO6Y'  # Use a new token if needed
 VIDEO_FOLDER = 'videos/barev/'
 
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('Բարև, ես բոտ եմ, ով բարևում է բոլոր նոր մասնակիցներին :)')
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Բարև, ես բոտ եմ, ով բարևում է բոլոր նոր մասնակիցներին:)')
-
-
-def new_member(update: Update, context: CallbackContext) -> None:
+async def new_member(update: Update, context: CallbackContext) -> None:
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Բարի գալուստ, {member.full_name}!')
+        await update.message.reply_text(f'Բարի գալուստ, {member.full_name}!')
+        video_file = random.choice(os.listdir(VIDEO_FOLDER))
+        video_path = os.path.join(VIDEO_FOLDER, video_file)
+        with open(video_path, 'rb') as video:
+            await context.bot.send_video(chat_id=update.message.chat_id, video=video)
 
-        try:
-            video_file = random.choice(os.listdir(VIDEO_FOLDER))
-            video_path = os.path.join(VIDEO_FOLDER, video_file)
-
-            with open(video_path, 'rb') as video:
-                context.bot.send_video(chat_id=update.message.chat_id, video=video)
-        except Exception as e:
-            print(f"Error sending video: {e}")
-
-
-def main():
-    updater = Updater(TOKEN)
-
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
-
-    updater.start_polling()
-    updater.idle()
-
+async def main():
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, new_member))
+    await application.run_polling()
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
